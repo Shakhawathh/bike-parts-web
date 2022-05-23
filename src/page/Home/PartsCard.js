889962ac68e4e8583b92cf2217dcc8e8e5398a40
Shakhawathh <shakhawathh55@gmail.com>
2022-05-24 04:25:25 +0600

    parts name changing

--- src/page/Home/PartsCard.js
+++ src/page/Home/PartsCard.js
@@ -5,14 +5,14 @@ const PartsCard = ({ service }) => {
   return (
     <div className="card  w-96 bg-[#96e2e2] shadow-xl ">
       <figure><img className='w-60' src={img} alt="" /></figure>
-      <div className="text-justify mx-4 mt-4">
-        <h2 className="card-title">Name: {name}</h2>
+      <div className="text-justify mx-4 mt-1">
+        <h2 className="card-title"><span className='text-xl font-semibold text-right '>Name:</span>{name}</h2>
         <p><span className='text-xl font-semibold text-right '>Description:</span> {description}</p>
         <p><span className='text-xl font-semibold text-right '>Minium order:</span> {min}</p>
         <p><span className='text-xl font-semibold text-right '>Available:</span> {available_quantity}</p>
         <p><span className='text-xl font-semibold text-right '>Per unit price</span>: ${price}</p>
-        <div className=" mt-6 justify-center w-auto">
-          <button className="btn btn-primary">Buy Now</button>
+        <div className=" mt-2 mr-2 justify-center ">
+          <button className="btn bg-[#001858] text-white">purchase</button>
         </div>
       </div>
     </div>
