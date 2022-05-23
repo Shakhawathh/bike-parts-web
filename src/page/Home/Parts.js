770fcc0550c534eb72d8a30535a2124ab39a8f2a
Shakhawathh <shakhawathh55@gmail.com>
2022-05-24 04:14:59 +0600

    parts information

--- src/page/Home/Parts.js
+++ src/page/Home/Parts.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PartsCard from './PartsCard';
-import helmat from '../../Image/parts/helmet.png'
-import brake from '../../Image/parts/brake.png'
-import tire from '../../Image/parts/tire.png'
+// import helmat from '../../Image/parts/helmet.png'
+// import brake from '../../Image/parts/brake.png'
+// import tire from '../../Image/parts/tire.png'
 
 const Parts = () => {
+
+    const [services, setServices] = useState([])
+    useEffect( ()=>{
+    fetch('Fake.json')
+    .then(res => res.json())
+    .then(data => setServices(data))
+    },[])
     return (
         <div>
             <h1 className='text-3xl font-bold text-indigo-900 mb-12 text-center '>Motocycle Parts</h1>
             <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 '>
-                <PartsCard img={helmat}></PartsCard>
-                <PartsCard img={brake}></PartsCard>
-                <PartsCard img={tire}></PartsCard>
+                {
+                    services.map(service=><PartsCard
+                    key={service._id}
+                    service={service}
+                    ></PartsCard>)
+                }
             </div>
         </div>
     );
--- src/page/Home/PartsCard.js
+++ src/page/Home/PartsCard.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
-const PartsCard = ({img}) => {
-    return (
-        <div className="card  w-96 bg-[#96e2e2] shadow-xl ">
-        <figure><img className='w-60' src={img} alt="Shoes" /></figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
+const PartsCard = ({ service }) => {
+  const { name, img, description, min, available_quantity, price } = service
+  return (
+    <div className="card  w-96 bg-[#96e2e2] shadow-xl ">
+      <figure><img className='w-60' src={img} alt="" /></figure>
+      <div className="text-justify mx-4 mt-4">
+        <h2 className="card-title">Name: {name}</h2>
+        <p><span className='text-xl font-semibold text-right '>Description:</span> {description}</p>
+        <p><span className='text-xl font-semibold text-right '>Minium order:</span> {min}</p>
+        <p><span className='text-xl font-semibold text-right '>Available:</span> {available_quantity}</p>
+        <p><span className='text-xl font-semibold text-right '>Per unit price</span>: ${price}</p>
+        <div className=" mt-6 justify-center w-auto">
+          <button className="btn btn-primary">Buy Now</button>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default PartsCard;
